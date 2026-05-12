bd727043a30c695cd44c839a3e5ca87bf0ef9de8
NMRenjoyer <dcreetz+github@proton.me>
2026-05-11 19:35:28 -0500

    first commit

---
+++ hosts/nixos-laptop/variables.nix
@@ -0,0 +1,39 @@
+{
+  config,
+  lib,
+  ...
+}: {
+  imports = [
+    # Choose your theme here:
+    ../../themes/shaymin.nix
+  ];
+
+  config.var = {
+    hostname = "nixos-laptop";
+    username = "dcreetz";
+    configDirectory = "/home/" + config.var.username + "/.config/nixos"; # The path of the nixos configuration directory
+
+    keyboardLayout = "us";
+
+    location = "Chicago";
+    timeZone = "America/Chicago";
+    defaultLocale = "en_US.UTF-8";
+    extraLocale = "en_US.UTF-8";
+
+    git = {
+      username = "NMRenjoyer";
+      email = "dcreetz+github@proton.me";
+    };
+
+    autoUpgrade = false;
+    autoGarbageCollector = true;
+  };
+
+  # DON'T TOUCH THIS
+  options = {
+    var = lib.mkOption {
+      type = lib.types.attrs;
+      default = {};
+    };
+  };
+}
