0decded4756c662634c675bcb093d40b5edc6048
NMRenjoyer <dcreetz+github@proton.me>
2026-05-12 13:08:25 -0500

    removed unused qutebrowser bookmarks

--- home/programs/helium/bookmarks/entertainment.nix
+++ home/programs/helium/bookmarks/entertainment.nix
@@ -8,14 +8,6 @@
         url = "https://youtube.com";
       }
       {
-        name = "Jellyfin";
-        url = "https://media.hadi.icu";
-      }
-      {
-        name = "Seerr";
-        url = "https://demandemedia.hadi.icu";
-      }
-      {
         name = "Social Media";
         icon = "group";
         bookmarks = [
@@ -32,16 +24,8 @@
             url = "https://reddit.com";
           }
           {
-            name = "Instagram";
-            url = "https://instagram.com";
-          }
-          {
             name = "Discord";
             url = "https://discord.com/channels/@me/";
-          }
-          {
-            name = "Gitlab";
-            url = "https://gitlab.com/";
           }
         ];
       }
