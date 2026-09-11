3941fea49cc99cf030768f00e44b243dea2814d2
NMRenjoyer <dcreetz+github@proton.me>
2026-09-10 22:52:45 -0500

    fixed escape in nvim and added new key

--- home/programs/tui/git/signing.nix
+++ home/programs/tui/git/signing.nix
@@ -1,14 +1,14 @@
 # This file is used to sign git commits using an SSH key.
 {
   # CHANGEDME: change this to your own SSH key.
-  home.file.".ssh/allowed_signers".text = "* ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINFwPSwaocKHy+rLRFURDU0fUHEAJxeda44ey2CAII+9";
+  home.file.".ssh/allowed_signers".text = "* ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA7pxE4EazA5LO1Q1hPlaoRP3OnTT02/bNZYDGIzyLBL";
 
   programs.git = {
     settings = {
       commit.gpgsign = true;
       gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
       gpg.format = "ssh";
-      user.signingkey = "~/.ssh/id_ed25519_signing.pub";
+      user.signingkey = "~/.ssh/id_ed25519_laptop.pub";
     };
   };
 }
